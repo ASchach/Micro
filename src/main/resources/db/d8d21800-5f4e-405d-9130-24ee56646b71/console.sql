1191c0a2b0fa1b16dfcac61feca423e76c2af010
Schach <anni619@student.sdu.dk>
2023-05-04 01:25:23 +0200

    revert changes, new DB

--- src/main/resources/db/d8d21800-5f4e-405d-9130-24ee56646b71/console.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO clients.clienttable (firstName, lastName, address, birthYear, CPR)
-VALUES ('lars', 'Larsen', 'svanevaenget 22', 1964, 0505642024),
-       ('Erik', 'Eriksen', 'Munkevaenget 17', 1980, 0202801321),
-       ('Soeren', 'Soeren', 'Drejebaenken 51', 1990, 0101906430);
